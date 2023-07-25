a5507c9574545faab55b18b6962aec2a38dc1cc0
Apple03244 <apple03244@gmail.com>
2023-07-25 21:36:44 +0900

    코딩테스트 모의고사 업로드

---
+++ 003_Coding수업자료/pac.py
@@ -0,0 +1,17 @@
+call = 15
+n = 0
+an = 3
+line =[]
+poly = []
+
+while True:
+    if call>0:
+        if call%2:
+            line.append(str(call))
+            poly = "".join(line)
+    else:
+        break
+    call-=1
+answer  = poly.count(str(an))
+
+print(answer)
